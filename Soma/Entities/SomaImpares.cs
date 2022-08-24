86d6164f9cf1adaf6553c109275f017f9350e18c
Nathalia Vieira <Silva.Nathalia@ibm.com>
2022-08-24 16:33:55 -0300

    first commit

---
+++ Soma/Entities/SomaImpares.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Soma.Calculo
+{
+    class SomaImpares
+    {
+        public int Numero1 { get; private set; }
+        public int Numero2 { get; private set; }
+        public int Soma { get; private set; }
+        public SomaImpares(int numero1, int numero2)
+        {
+            Numero1 = numero1;
+            Numero2 = numero2;
+        }
+        public int SomaDeImpares()
+        {
+            if (Numero1 < Numero2)
+            {
+                for (int i = Numero1 + 1; i <= Numero2; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Soma = i;
+                    }
+                }
+            }
+            else
+            {
+                for (int i = Numero2 + 1; i < Numero1; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Soma += i;
+                    }
+                }
+            }
+            return Soma;
+        }
+    }
+}
---
+++ Soma/Program.cs
@@ -0,0 +1,19 @@
+﻿using Soma.Calculo;
+using System;
+
+namespace Soma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            var numero1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite um número inteiro: ");
+            var numero2 = int.Parse(Console.ReadLine());
+
+            SomaImpares resultado = new SomaImpares(numero1, numero2);
+            Console.WriteLine(resultado.SomaDeImpares());
+        }
+    }
+}
